7801fabd5184bb92232f7d41aceb54de1de33224
Edin Ramírez <22ramirez.edin@gmail.com>
2020-04-12 14:48:59 -0500

    Theme to index

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,11 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="pink darken-3"
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h1>Pitts & Collies</h1>
       </div>
 
       <v-spacer></v-spacer>
@@ -38,19 +22,19 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Index/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Index from './components/Index';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Index,
   },
 
   data: () => ({
---
+++ src/components/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="6" class="themeColumn">
+        <h1 class="display-3 font-weight-bold mb-2">
+          Pitts & Collies
+        </h1>
+        <h3 class="display-1 mb-3">
+          Cuidado de la salud de tus mascotas
+        </h3>
+      </v-col>
+      <v-col cols="6">
+        <h3 class="display-3 mb-3">
+          Formulario
+        </h3>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+  }
+</script>
+<style>
+.themeColumn {
+background: #EC407A;
+color: #FFFFFF;
+}
+</style>
